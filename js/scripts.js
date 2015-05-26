b283a482cb6bba64c7888e72f8d96aa84ce5e70f
Guest User <student@epicodus.com>
2015-05-26 13:48:57 -0700

    add webpage to allow for user input and return correct answer.

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,4 @@
 var factorial = function(number) {
-  var theAnswer = 1;
-  var i = 0;
-  if (number > 0) {
-    for (i = 0; i < number; i++) {
-      theAnswer = theAnswer * (number - i);
-    }
-  }
-  return theAnswer;
-};
-
-var factorial = function(number) {
 var theAnswer = 0
   if (number > 0 ) {
     theAnswer = number * factorial(number - 1)
@@ -17,3 +6,19 @@ var theAnswer = 0
     theAnswer = 1
 return theAnswer
 };
+
+$(document).ready(function() {
+  $("form#factorials").submit(function(event) {
+    $(".entered_number").empty();
+    $(".factorial_number").empty();
+    var number = parseInt($("input#input_number").val());
+    var result = factorial(number);
+
+    $(".entered_number").text(number);
+    $(".factorial_number").text(result);
+
+    $(".results").show();
+    event.preventDefault();
+
+  });
+});
